b21ec9b729cf653e6edccd42609b7a3366eb0e6d
evilBetooxx <albertof4in@gmail.com>
2023-11-16 15:11:14 -0600

    Pagos con PayPal finalizados

--- src/controllers/payment.controller.js
+++ src/controllers/payment.controller.js
@@ -1,7 +1,5 @@
 import Stripe from "stripe";
-
-const STRIPE_SECRET_KEY = process.env.STRIPE_SECRET_KEY;
-const stripe = new Stripe(STRIPE_SECRET_KEY);
+import axios from "axios";
 
 export const createSession = async (req, res) => {
   // const { price, name, description, image } = req.body;
@@ -28,6 +26,8 @@ export const createSession = async (req, res) => {
     });
 
     */
+  const STRIPE_SECRET_KEY = process.env.STRIPE_SECRET_KEY;
+  const stripe = new Stripe(STRIPE_SECRET_KEY);
 
   const session = await stripe.checkout.sessions.create({
     line_items: [
@@ -50,3 +50,72 @@ export const createSession = async (req, res) => {
 
   res.json(session);
 };
+
+export const createOrder = async (req, res) => {
+  const order = {
+    intent: "CAPTURE",
+    purchase_units: [
+      {
+        amount: {
+          currency_code: "MXN",
+          value: "100.00",
+        },
+      },
+    ],
+    application_context: {
+      brand_name: "Barhalla",
+      landing_page: "NO_PREFERENCE",
+      user_action: "PAY_NOW",
+      return_url: `${process.env.HOST}/capture-order`,
+      cancel_url: `${process.env.HOST}/cancel-order`,
+    },
+  };
+
+  const params = new URLSearchParams();
+  params.append("grant_type", "client_credentials");
+
+  const {
+    data: { access_token },
+  } = await axios.post(`${process.env.PAYPAL_API}/v1/oauth2/token`, params, {
+    auth: {
+      username: process.env.PAYPAL_CLIENT_ID,
+      password: process.env.PAYPAL_SECRET_KEY,
+    },
+  });
+
+  const response = await axios.post(
+    `${process.env.PAYPAL_API}/v2/checkout/orders`,
+    order,
+    {
+      headers: {
+        Authorization: `Bearer ${access_token}`,
+      },
+    }
+  );
+
+  console.log(response.data);
+
+  return res.json("ok");
+};
+
+export const captureOrder = async (req, res) => {
+  const { token } = req.query;
+  try {
+    const response = await axios.post(
+      `${process.env.PAYPAL_API}/v2/checkout/orders/${token}/capture`,
+      {},
+      {
+        auth: {
+          username: process.env.PAYPAL_CLIENT_ID,
+          password: process.env.PAYPAL_SECRET_KEY,
+        },
+      }
+    );
+    console.log(response.data);
+    return res.send(response.data);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const cancelOrder = async (req, res) => {};
--- src/routes/payment.routes.js
+++ src/routes/payment.routes.js
@@ -1,11 +1,15 @@
 import { Router } from "express";
-import { createSession } from "../controllers/payment.controller.js";
+import { createSession, createOrder, captureOrder, cancelOrder } from "../controllers/payment.controller.js";
 
 const router = Router();
 
 router.get('/create-checkout-session', createSession)
 router.get('/succes', (req, res) => res.redirect('/') )
 router.get('/cancel', (req, res) => res.redirect('/') )
+
+router.get('/create-order', createOrder)
+router.get('/capture-order', captureOrder )
+router.get('/cancel-order', cancelOrder )
 
 
 
